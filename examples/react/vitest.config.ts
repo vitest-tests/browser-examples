acd6b5dc0fd81c3938230171734937132eaa67c0
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-06-24 16:18:50 +0200

    feat: add react example

---
+++ examples/react/vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    browser: {
+      enabled: true,
+      name: 'chromium',
+      provider: 'playwright',
+    },
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+})
